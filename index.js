237aaaa6dead0c605c6c64f16c1c1115e494116b
PaulNguyen <paulnguyen2207@gmail.com>
2021-10-12 10:41:48 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+
+let cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    console.log (cats);   
+};
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    console.log (cats);   
+};
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    console.log (cats);   
+};
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    console.log (cats);   
+};
+
+
+function appendCat(name) {
+return [...cats,name]
+     
+};
+
+
+function prependCat(name) {
+   return  [name,...cats]
+      
+};
+
+
+function removeLastCat() {
+    return cats.slice(0,-1);
+   
+};
+
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
